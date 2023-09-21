3f40fce46d961ceae320d6229393b8525449e4fd
lelyta30 <salsabilalelyta@gmail.com>
2023-09-22 03:14:56 +0700

    alhamdulillah

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,35 +4,17 @@
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- Bootstrap CSS -->
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>SMS Portal</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-        <title>SMS Portal - Laravel 8 & Twilio</title>
     </head>
     <body class="bg-light">
-        <div class="container mt-5">
-            <h1 class="text-center">SMS Portal - Laravel 8 & Twilio</h1>
-            <div class="row p-4 border rounded-3 bg-body">
-                <div class="col-6">
-                    <div class="card">
-                        <div class="card-header">
-                            Add Phone Number
-                        </div>
-                        <div class="card-body">
-                            <form action="/" method="POST">
-                                @csrf
-                                <div class="form-group">
-                                    <label>Enter Phone Number</label>
-                                    <input type="tel" class="form-control" name="phone" placeholder="+6282xxxxxxx">
-                                </div>
-                                <button type="submit" class="btn btn-primary mt-2">Add Contact</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-6">
-                    <div class="card">
-                        <div class="card-header">
+    <div class="container mt-5">
+        <div class="row p-4 border rounded-3 bg-body">
+            <div class="col-12">
+                <div class="card">
+                                            <div class="card-header">
                             Send SMS
                         </div>
                         <div class="card-body">
@@ -47,8 +29,8 @@
                                     </select>
                                 </div>
                                 <div class="form-group">
-                                    <label>Message</label>
-                                    <textarea name="body" class="form-control" rows="3"></textarea>
+                                <label>Message</label>
+    <textarea name="body" class="form-control" rows="3">APA</textarea>
                                 </div>
                                 <button type="submit" class="btn btn-primary mt-2">Send</button>
                             </form>
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,35 +3,39 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
     <link rel="stylesheet" href="//cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
-    <title>Data Posts</title>
+    <title>Statistik Pelanggan</title>
   </head>
 
   <body>
 
-    <div class="container" style="margin-top: 80px">
+<div class="container mt-5"> 
       <div class="row">
         <div class="col-md-12">
           <div class="card">
             <div class="card-header">
-              DATA POSTS
-            </div>
-            <div class="card-body">
-              <a href="{{ route('posts.create') }}" class="btn btn-md btn-success" style="margin-bottom: 10px">TAMBAH DATA</a>
+            DETAIL STATISTIK PELANGGAN            
+          </div>
+          <div class="card-body">
+          <a href="{{ route('posts.create') }}" class="btn btn-md btn-success mb-2">CREATE</a>
+              <div class="table-responsive">
               <table class="table table-bordered" id="myTable">
                 <thead>
-                  <tr>
-                    <th scope="col">PHONE</th>
-                    <th scope="col">CONTENT</th>
-                    <th scope="col">AKSI</th>
-                  </tr>
+                <tr>
+    <th scope="col"><input type="checkbox" id="selectAllCheckbox"></th>
+    <th scope="col">PHONE</th>
+    <th scope="col">CONTENT</th>
+    <th scope="col">AKSI</th>
+</tr>
                 </thead>
                 <tbody>
 
 
                   @foreach($posts as $post)
                   <tr>
+                  <td><input type="checkbox" name="selected[]" value="{{ $post->id }}" class="dataTerpilihCheckbox"></td> <!-- Kotak centang -->
                       <td>{{ $post->phone }}</td>
                       <td>{!! $post->content !!}</td>
                       <td class="text-center">
@@ -53,6 +57,8 @@
       </div>
     </div>
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/twilio@3.69.0/dist/twilio.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
     <script src="//cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
@@ -60,6 +66,31 @@
       $(document).ready( function () {
           $('#myTable').DataTable();
       } );
+      $(document).ready(function () {
+        $('#myTable').DataTable();
+
+        // Fungsi untuk menangani "Centang Semua"
+        $("#selectAllCheckbox").on("change", function () {
+            var isChecked = $(this).prop("checked");
+            $(".dataTerpilihCheckbox").prop("checked", isChecked);
+            updateSelectedPhones();
+        });
+
+        $(".dataTerpilihCheckbox").on("change", function () {
+            updateSelectedPhones();
+        });
+
+        function updateSelectedPhones() {
+            var selectedPhones = [];
+            $(".dataTerpilihCheckbox:checked").each(function () {
+                var phone = $(this).closest("tr").find("td:eq(1)").text();
+                selectedPhones.push(phone);
+            });
+
+            // Memperbarui isi div dengan nomor telepon yang dipilih
+            $("#selectedPhones").text(selectedPhones.join(", "));
+        }
+    });
     </script>
   </body>
 </html>
